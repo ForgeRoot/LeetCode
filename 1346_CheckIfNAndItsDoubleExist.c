4ea07fd742572f2c0cb6da3b50a682ebcdfdc8ed
ForgeRoot <dylan.van.eck@capgemini.com>
2025-02-03 16:07:38 +0100

    completed problem 1346 - BruteForce

---
+++ 1346_CheckIfNAndItsDoubleExist.c
@@ -0,0 +1,95 @@
+// URL: https://leetcode.com/problems/check-if-n-and-its-double-exist/description/
+// 
+// 1346. Check If N and Its Double Exist
+// 
+// Given an array arr of integers, check if there exist two indices i and j such that :
+// 
+// 	i != j
+// 	0 <= i, j < arr.length
+// 	arr[i] == 2 * arr[j]
+// 
+//  
+// Example 1:
+// 
+// Input: arr = [10,2,5,3]
+// Output: true
+// Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+// 
+// Example 2:
+// 
+// Input: arr = [3,1,7,11]
+// Output: false
+// Explanation: There is no i and j that satisfy the conditions.
+// 
+//  
+// Constraints:
+// 
+// 	2 <= arr.length <= 500
+// 	-103 <= arr[i] <= 103
+
+bool checkIfExist(int* arr, int arrSize) {
+  for(int i = 0; i < arrSize - 1; i++){
+    int check = arr[i];
+    for(int j = i+1; j < arrSize; j++){
+      int new_int = arr[j];
+      if (check != 0 && arr[j] != 0) {
+        if (check/arr[j] == 2 && check%arr[j] == 0)
+          return true;
+      }
+      if (check*2 == arr[j])
+          return true;
+    }
+  }
+  return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
